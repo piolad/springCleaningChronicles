c86febaa8d17a9fc254f8b4daeb98c679b5b645f
Piotr Ladkowski <piolad2003@gmail.com>
2025-03-13 15:25:34 +0100

    add simple demo endpoints and SecurityConfig1 to test security

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,30 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("hello")
+	public String hello(){
+		return "Hello World";
+	}
+
+	@GetMapping("ay")
+	public String ay(){
+		return "ayy";
+	}
+
+
+	@GetMapping("public/sayHi")
+	public String hi(){
+		return "hi";
+	}
 }
---
+++ src/main/java/com/example/demo/SecurityConfig1.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig1 {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // Configure which requests are allowed without authentication
+                .authorizeHttpRequests(auth -> auth
+                        // Anyone can access paths starting with /public/
+                        // You can have multiple matchers if desired
+                        .requestMatchers("/public/**", "/css/**", "/js/**", "/images/**", "/hello","/login").permitAll()
+
+
+                        // For everything else, authentication is required
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .rememberMe(Customizer.withDefaults());
+
+
+        // Return the built HttpSecurity object
+        return http.build();
+    }
+}
